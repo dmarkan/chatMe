56bf5939a4a5fcbe2982852a663836da4e4681f8
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-12-26 13:42:55 +0100

    database completed, post and del button

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb+srv://darko:darmar1986@cluster0.62x42.mongodb.net/chatMeDB?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://darko:darmar1986@cluster0.62x42.mongodb.net/chatMeDB?retryWrites=true&w=majority", function(err) {
+    // If no error, successfully connected
+  });
 
 const messagesSchema = {
     text: String
@@ -16,19 +18,48 @@ const messagesSchema = {
 
 const Message = mongoose.model("Message", messagesSchema);
 
+const defaultMessages = [];
+
 app.get("/", function (req, res) {
-    res.render("index.ejs")
-})
-
-app.post("/", function (req, res) {
-    const messageContent = req.body.newMessage;
-
-    const message = new Message ({
-        text: messageContent
+    Message.find({}, function (err, foundMessages) {
+        if (foundMessages.length === 0) {
+            Message.insertMany(defaultMessages, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully saved messages into the DB");
+                }
+            });
+            res.redirect("/");
+        } else {
+        res.render('index.ejs', {
+            newMessages: foundMessages
+        });
+    }
     });
 
-    message.save();
-    res.redirect("/");
+    app.post("/", function (req, res) {
+        const messageContent = req.body.newMessage;
+    
+        const message = new Message ({
+            text: messageContent
+        });
+    
+        message.save();
+        res.redirect("/");
+    })
+    
+    app.post("/delete", function (req, res) {
+        const messageId = req.body.buttonDel;
+        Message.findByIdAndRemove(messageId, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Message successfully removed");
+                res.redirect("/");
+            }
+    });
+    })
 })
 
 app.listen(3000, function () {
