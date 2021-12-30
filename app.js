4f79b4be5846c75f491acf2ecb07daa525f69ed8
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-12-30 17:48:12 +0100

    image upload added

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const path = require('path');
+const fs = require("fs");
+const multer = require("multer");
+
+var imageModel = require('./models/imageModel');
 const app = express();
 
 app.set("view engine", "ejs");
@@ -22,6 +27,17 @@ const messagesSchema = {
 const Message = mongoose.model("Message", messagesSchema);
 
 const defaultMessages = [];
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+ 
+var upload = multer({ storage: storage })
 
 app.get("/", function (req, res) {
     Message.find({}, function (err, foundMessages) {
@@ -48,7 +64,6 @@ app.get("/", function (req, res) {
             text: messageContent,
             date: messageDate
         });
-
         message.save();
         res.redirect("/");
     })
@@ -63,7 +78,27 @@ app.get("/", function (req, res) {
                 res.redirect("/");
             }
         });
-    })
+    });
+
+    app.post("/uploadphoto",upload.single('myImage'),(req,res)=>{
+        var img = fs.readFileSync(req.file.path);
+        var encode_img = img.toString('base64');
+        var final_img = {
+            contentType:req.file.mimetype,
+            image:Buffer.from(encode_img,'base64')
+        };
+        imageModel.create(final_img,function(err,result){
+            if(err){
+                console.log(err);
+            }else{
+                console.log(result.img.Buffer);
+                console.log("Image saved To database");
+                res.contentType(final_img.contentType);
+                // res.send(final_img.image);
+                res.redirect("/");
+            }
+        });
+    });
 })
 
 app.listen(process.env.PORT || 3000, function () {
